cb2ab6ad84e352fc9edca7969a1e279d2a0a6fef
aliyasser20 <aliyasser20@hotmail.com>
2020-04-23 13:57:59 -0400

    update src files, setup router, setup redux

---
+++ src/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div className="App">
+    <h1>Three-tutorial</h1>
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from "./App/App";
+
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, composeEnhancers());
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
