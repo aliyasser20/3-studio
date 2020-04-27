dd907255336566034fb044eb260e2e3dc9e7a0b4
aliyasser20 <aliyasser20@hotmail.com>
2020-04-27 02:38:14 -0400

    figured how to change part color

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,65 @@
 import React from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import Car from "./Models/Car/Car";
 import Rocket from "./Models/Rocket/Rocket";
 import Duck from "./Models/Duck/Duck";
 
 const App = () => (
+  // let scene;
+  // let camera;
+  // let renderer;
+  // let controls;
+
+  // const animate = () => {
+  //   renderer.render(scene, camera);
+  //   requestAnimationFrame(animate);
+  // };
+
+  // const init = () => {
+  //   scene = new THREE.Scene();
+  //   scene.background = new THREE.Color(0xdddddd);
+
+  //   camera = new THREE.PerspectiveCamera(
+  //     60,
+  //     window.innerWidth / window.innerHeight,
+  //     1,
+  //     5000
+  //   );
+  //   camera.position.set(0, 0, 25);
+  //   // controls = new OrbitControls(camera);
+  //   scene.add(new THREE.AxesHelper(500));
+  //   renderer = new THREE.WebGLRenderer();
+  //   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //   document.body.appendChild(renderer.domElement);
+  //   animate();
+  // };
+
+  // init();
+
+  // new GLTFLoader().load("/models/rocket/rocket.glb", result => {
+  //   const model = result.scene.children[0];
+  //   console.log(model);
+  //   scene.add(model);
+  //   animate();
+  // });
+
+  // const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+  // scene.add(hemiLight);
+
   <div className="App">
-    <Duck />
-    {/* <Car /> */}
+    {/* <Duck /> */}
+    <Car />
     {/* <Rocket /> */}
   </div>
 );
-
 export default App;
+
+// <div className="App">
+//   <Duck />
+//   {/* <Car /> */}
+//   {/* <Rocket /> */}
+// </div>
--- src/App/Model/Model.js
+++ src/App/Model/Model.js
@@ -6,31 +6,29 @@ import * as THREE from "three";
 const Model = props => {
   const [model, setModel] = useState();
 
-  const createModel = (geometry, materials) => {
-    const mesh = new THREE.Mesh(
-      geometry,
-      new THREE.MeshFaceMaterial(materials)
-    );
-    mesh.scale.set(150, 150, 150);
-    mesh.position.y = 0;
-    mesh.position.x = 0;
-    mesh.rotation.x = 340;
+  const createModel = gltf => {
+    const theModel = gltf.scene;
+    const INITIAL_MTL = new THREE.MeshPhongMaterial({
+      color: 0x008000,
+      shininess: 10
+    });
 
-    console.log(geometry);
+    console.log(theModel);
+    theModel.traverse(o => {
+      if (o.isMesh && o.name === "Body_carbon_fibre_0") {
+        console.log(o);
+        o.material = INITIAL_MTL;
+      }
+    });
 
-    setModel(mesh);
+    setModel(gltf.scene);
   };
 
   useEffect(() => {
-    new GLTFLoader().load(props.url, setModel);
+    new GLTFLoader().load(props.url, createModel);
   }, [props.url]);
 
-  console.log(model);
-
-  // const { nodes, materials, animations } = useLoader(GLTFLoader, props.url);
-  // console.log(nodes, materials, animations);
-
-  return model ? <primitive object={model.scene} /> : null;
+  return model ? <primitive object={model} /> : null;
 };
 
 export default Model;
