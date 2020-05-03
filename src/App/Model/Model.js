631ca0fa64bee997690641b78540b5799ecaa97f
aliyasser20 <aliyasser20@hotmail.com>
2020-05-03 06:31:00 -0400

    refacator Model js file

--- src/App/Model/Model.js
+++ src/App/Model/Model.js
@@ -1,22 +1,61 @@
-import React, { useState, useEffect, useRef, Suspense } from "react";
-import { Canvas, useThree, useFrame, Dom } from "react-three-fiber";
+import React, { useState, useEffect, Suspense } from "react";
+import { Canvas, Dom } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 
 import Controls from "../Controls/Controls";
-import Scene from "../Scene/Scene";
 import Environment from "../Enivronment/Environment";
 
 import createModel from "../../helpers/create_model";
 
 const Model = props => {
+  // ! State ------------------------------------------------- //
+  // ? Model, bounding box, zoom, center states //
   const [model, setModel] = useState(null);
   const [box, setBox] = useState();
+  const [fov, setFov] = useState(45);
   const [far, setFar] = useState(0);
   const [near, setNear] = useState(0);
   const [sizeBounding, setSizeBounding] = useState({ x: 0, y: 0, z: 0 });
-  const [environment, setEnvironment] = useState(null);
 
+  // ? Environment & background states //
+  const [environment, setEnvironment] = useState(null);
+  const [bgSolid, setBgSolid] = useState(true);
+  const [bgEnvironment, setBgEnvironment] = useState(false);
+  const [mapEnvironment, setMapEnvironment] = useState(true);
+  const [bgColor, setBgColor] = useState("555555");
+
+  // ? Lights states //
+  // Ambient
+  const [ambient, setAmbient] = useState(true);
+  const [ambientColor, setAmbientColor] = useState("ffffff");
+  const [ambientIntensity, setAmbientIntensity] = useState(0.3);
+
+  // Hemisphere
+  const [hemisphere, setHemisphere] = useState(true);
+  const [hemisphereColor, setHemisphereColor] = useState("ffffff");
+  const [hemisphereIntensity, setHemisphereIntensity] = useState(1);
+
+  // Directional
+  const [directional, setDirectional] = useState(true);
+  const [directionalColor, setDirectionalColor] = useState("ffffff");
+  const [directionalIntensity, setDirectionalIntensity] = useState(
+    0.8 * Math.PI
+  );
+  const [directionalPosition, setDirectionalPosition] = useState([
+    0.5,
+    0,
+    0.86
+  ]);
+
+  // ? Additional helpers and controls states //
+  const [showAxis, setShowAxis] = useState(false);
+  const [showBoundingBox, setShowBoundingBox] = useState(false);
+  const [allowOrbitControls, setAllowOrbitControls] = useState(true);
+  const [autoRotate, setAutoRotate] = useState(false);
+
+  // ! ------------------------------------------------- //
+  // ? Load model with materials
   useEffect(() => {
     new GLTFLoader().load(props.url, gltf =>
       createModel(
@@ -31,71 +70,74 @@ const Model = props => {
     );
   }, [props.url]);
 
-  console.log(environment);
-
-  // function Main() {
-  //   const scene = useRef();
-  //   const { camera } = useThree();
-  //   useFrame(
-  //     ({ gl }) =>
-  //       void ((gl.autoClear = true), gl.render(scene.current, camera)),
-  //     100
-  //   );
-  //   return <scene ref={scene}>{/* ... */}</scene>;
-  // }
-
-  const fallback = (
+  // ? Fallback case
+  const fallbackElement = (
     <Dom>
-      <div>🤤</div>
+      <div></div>
     </Dom>
   );
 
-  const output = model ? (
+  // ? Canvas output
+  const canvasElement = model ? (
     <Canvas
-      // environment={environment}
-      // shadowMap
-      // gl={{ toneMapping: 0.1 }}
-      // gl={{ environment }}
       camera={{
         position: [-sizeBounding.x, sizeBounding.y, sizeBounding.z],
-        fov: 45,
+        fov,
         far,
         near
       }}
       onCreated={({ gl, scene }) => {
         gl.toneMapping = THREE.ACESFilmicToneMapping;
         gl.outputEncoding = THREE.sRGBEncoding;
-        scene.background = new THREE.Color("#373737");
-        // scene.background = new THREE.Color("#cacaca");
-        // scene.background = environment;
-        // scene.environment = environment;
-        // scene.background.convertSRGBToLinear()
+        gl.gammaFactor = 2.2;
+        gl.gammaOutput = true;
+
+        if (bgSolid) {
+          scene.background = new THREE.Color(`#${bgColor}`);
+        }
+
+        if (bgEnvironment) {
+          scene.background = environment;
+        }
       }}
     >
-      {/* <pointLight intensity={1} position={[1000, 1000, 1000]} /> */}
-      {/* <spotLight intensity={1} position={[1000, 1000, 1000]} />
-      <spotLight intensity={1} position={[-1000, -1000, -1000]} /> */}
-      <directionalLight intensity={0.8 * Math.PI} position={[0.5, 0, 0.86]} />
-      {/* <directionalLight intensity={0.8 * Math.PI} position={[-0.5, 0, -0.86]} /> */}
-      {/* <pointLight intensity={1} position={[-1000, -1000, -1000]} /> */}
-      {/* <hemisphereLight /> */}
-      <ambientLight intensity={0.3} color={0xffffff} />
-      {/* <pointLight intensity={1} position={[-50, 50, -50]} /> */}
-      {/* <pointLight intensity={1} position={[0, 100, -500]} /> */}
-      <Controls />
-      {/* <axesHelper scale={[200, 200, 200]} /> */}
       <primitive object={model} />
-      <Suspense fallback={fallback}>
-        {/* <Scene /> */}
-        <Environment />
-      </Suspense>
-      {/* <primitive object={environment} /> */}
-      {/* <boxHelper object={box} /> */}
+      {directional && (
+        <directionalLight
+          intensity={directionalIntensity}
+          color={`#${directionalColor}`}
+          position={directionalPosition}
+        />
+      )}
+      {hemisphere && (
+        <hemisphereLight
+          intensity={hemisphereIntensity}
+          color={`#${hemisphereColor}`}
+        />
+      )}
+      {ambient && (
+        <ambientLight intensity={ambientIntensity} color={`#${ambientColor}`} />
+      )}
+      {mapEnvironment && (
+        <Suspense fallback={fallbackElement}>
+          <Environment />
+        </Suspense>
+      )}
+      {allowOrbitControls && <Controls autoRotate={autoRotate} />}
+      {showAxis && (
+        <axesHelper
+          scale={[
+            1.2 * sizeBounding.x,
+            1.2 * sizeBounding.y,
+            1.2 * sizeBounding.z
+          ]}
+        />
+      )}
+      {showBoundingBox && <boxHelper object={box} />}
     </Canvas>
   ) : null;
 
-  return <div>{output}</div>;
-  // return <Main />;
+  return <div>{canvasElement}</div>;
 };
 
 export default Model;
