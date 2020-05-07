97d0c6fbb2a622051f008a63338f22dc61973500
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 11:38:28 -0400

    add container to navbar

--- src/App/NavBar/NavBar.js
+++ src/App/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 
 import red from "@material-ui/core/colors/red";
 import grey from "@material-ui/core/colors/grey";
@@ -15,13 +16,15 @@ import themeCreator from "../../helpers/themeCreator";
 const NavBar = () => (
   <ThemeProvider theme={themeCreator(grey[900], purple[800])}>
     <AppBar position="fixed" color="primary">
-      <Toolbar>
-        <Typography variant="h5">
-          <div className="text-color-grad">
-            <Box fontWeight="700">Final Project</Box>
-          </div>
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography variant="h5">
+            <div className="text-color-grad">
+              <Box fontWeight="700">Final Project</Box>
+            </div>
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
   </ThemeProvider>
 );
