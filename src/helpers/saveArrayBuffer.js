0859c65b4b19ccf205636995f4adf03ac44ca452
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 16:08:49 -0400

    comments

--- src/helpers/saveArrayBuffer.js
+++ src/helpers/saveArrayBuffer.js
@@ -1,5 +1,6 @@
 import save from "./save";
 
+// Function that saves .glb
 const saveArrayBuffer = (buffer, filename) => {
   save(new Blob([buffer], { type: "application/octet-stream" }), filename);
 };
