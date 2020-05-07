3ab4e73e73845ecd34db23c8962bd8f2aecb146c
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 16:11:56 -0400

    comments

--- src/helpers/themeCreator.js
+++ src/helpers/themeCreator.js
@@ -2,6 +2,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
 import green from "@material-ui/core/colors/green";
 
+// Function that creates custom theme object for material UI
 const themeCreator = (primary = blue[500], secondary = green[500]) => {
   const theme = createMuiTheme({
     palette: {
