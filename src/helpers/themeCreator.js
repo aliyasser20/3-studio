4acf12daffdd91385e3f6d7a65585e9143b39911
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 15:38:51 -0400

    cleanup

--- src/helpers/themeCreator.js
+++ src/helpers/themeCreator.js
@@ -6,8 +6,7 @@ const themeCreator = (primary = blue[500], secondary = green[500]) => {
   const theme = createMuiTheme({
     palette: {
       primary: {
-        main: primary,
-        mainGradient: "linear-gradient(#F45C43, #EB3349)"
+        main: primary
       },
       secondary: {
         main: secondary
