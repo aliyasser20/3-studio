912ba091caa2d328e2ac83f00470763800c851f0
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 12:53:10 -0400

    buttons in navbar

--- src/App/NavBar/NavBar.js
+++ src/App/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider, useTheme } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
@@ -14,26 +14,44 @@ import themeCreator from "../../helpers/themeCreator";
 
 import "./NavBar.scss";
 
-const NavBar = () => (
-  <ThemeProvider theme={themeCreator(grey[900])}>
-    <AppBar position="fixed" color="primary">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <div className="navbar-flex">
-            <Typography variant="h5">
-              <div className="text-color-grad">
-                <Box fontWeight="700">Final Project</Box>
-              </div>
-            </Typography>
-            <Box>
-              <Button color="inherit">Login</Button>
-              <Button color="inherit">Signup</Button>
-            </Box>
-          </div>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  </ThemeProvider>
-);
+const NavBar = () => {
+  const theme = useTheme();
+
+  console.log(theme);
+
+  return (
+    <ThemeProvider theme={themeCreator(grey[900])}>
+      <AppBar position="fixed" color="primary">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <div className="navbar-flex">
+              <Typography variant="h5">
+                <div className="text-color-grad">
+                  <Box fontWeight="700">Final Project</Box>
+                </div>
+              </Typography>
+              <Box>
+                <Button color="inherit" href="#text-buttons">
+                  <Box fontWeight="700">Login</Box>
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    background:
+                      "linear-gradient(rgb(244, 92, 67), rgb(235, 51, 73))",
+                    marginLeft: "1.5em"
+                  }}
+                >
+                  <Box fontWeight="700">Signup</Box>
+                </Button>
+              </Box>
+            </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
 
 export default NavBar;
