1c8295a1f2d92d514d94c6dd21ddbeae7b318b8e
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 16:17:36 -0400

    cleanup

--- src/App/Studio/Modes/Edit/UserControls/UserControls.js
+++ src/App/Studio/Modes/Edit/UserControls/UserControls.js
@@ -1,28 +1,23 @@
 import React from "react";
 
-import { useTheme } from "@material-ui/core/styles";
 import GetAppRounded from "@material-ui/icons/GetAppRounded";
-
 import Button from "@material-ui/core/Button/Button";
 
 import fileExporter from "../../../../../helpers/fileExporter";
 
-const UserControls = () => {
-  const theme = useTheme();
-  return (
-    <div>
-      <span className="gradient-button">
-        <Button
-          variant="contained"
-          color="primary"
-          startIcon={<GetAppRounded />}
-          onClick={fileExporter}
-        >
-          Download
-        </Button>
-      </span>
-    </div>
-  );
-};
+const UserControls = () => (
+  <div>
+    <span className="gradient-button">
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<GetAppRounded />}
+        onClick={fileExporter}
+      >
+        Download
+      </Button>
+    </span>
+  </div>
+);
 
 export default UserControls;
