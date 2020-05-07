725e6885633711cd072db91bc5b2e8434fe7a0dc
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 11:51:33 -0400

    flex navbar

--- src/App/NavBar/NavBar.js
+++ src/App/NavBar/NavBar.js
@@ -6,21 +6,30 @@ import Typography from "@material-ui/core/Typography";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 
 import grey from "@material-ui/core/colors/grey";
 
 import themeCreator from "../../helpers/themeCreator";
+
+import "./NavBar.scss";
 
 const NavBar = () => (
   <ThemeProvider theme={themeCreator(grey[900])}>
     <AppBar position="fixed" color="primary">
       <Container maxWidth="xl">
         <Toolbar>
-          <Typography variant="h5">
-            <div className="text-color-grad">
-              <Box fontWeight="700">Final Project</Box>
-            </div>
-          </Typography>
+          <div className="navbar-flex">
+            <Typography variant="h5">
+              <div className="text-color-grad">
+                <Box fontWeight="700">Final Project</Box>
+              </div>
+            </Typography>
+            <Box>
+              <Button color="inherit">Login</Button>
+              <Button color="inherit">Signup</Button>
+            </Box>
+          </div>
         </Toolbar>
       </Container>
     </AppBar>
