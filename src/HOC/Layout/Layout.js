a32d28bcc3a11d354631feada08dc8df58fc2e4e
aliyasser20 <aliyasser20@hotmail.com>
2020-05-07 10:33:51 -0400

    create HOC layout

---
+++ src/HOC/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+import "./Layout.scss";
+
+class Layout extends React.Component {
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default Layout;
