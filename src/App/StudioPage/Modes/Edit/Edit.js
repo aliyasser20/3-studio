4b48b872e0b4b692ebf876a89e35216580dc9b93
Jaybur1 <jayburbyga@gmail.com>
2020-05-08 17:48:22 -0400

    edit

--- src/App/StudioPage/Modes/Edit/Edit.js
+++ src/App/StudioPage/Modes/Edit/Edit.js
@@ -5,11 +5,7 @@ import UserControls from "./UserControls/UserControls";
 
 const Edit = () => (
   <div className="edit">
-<<<<<<< HEAD
-    <Model url="models/car.glb" />
-=======
     <Model url="/models/duck.glb" />
->>>>>>> cd28da11ce1e4e680d81ecd27e54f7fa01a3e872
     <UserControls />
   </div>
 );
