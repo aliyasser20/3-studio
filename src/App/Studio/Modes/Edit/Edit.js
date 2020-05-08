3922401f0db8348414bb47da636b3809f7c21ae7
Jaybur1 <jayburbyga@gmail.com>
2020-05-08 15:49:48 -0400

    file case change

--- src/App/Studio/Modes/Edit/Edit.js
+++ src/App/Studio/Modes/Edit/Edit.js
@@ -5,7 +5,7 @@ import UserControls from "./UserControls/UserControls";
 
 const Edit = () => (
   <div className="edit">
-    <Model url="/models/car.glb" />
+    <Model url="Models/car.glb" />
     <UserControls />
   </div>
 );
