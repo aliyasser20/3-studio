cf569caf851f55ec6972c7f41735b65706743c98
aliyasser20 <aliyasser20@hotmail.com>
2020-05-08 15:48:05 -0400

    all initial routes set up

--- src/App/App.js
+++ src/App/App.js
@@ -4,16 +4,20 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Layout from "./HOC/Layout/Layout";
 import LandingPage from "./LandingPage/LandingPage";
 import LoginPage from "./Login_Signup/LoginPage/LoginPage";
+import SignupPage from "./Login_Signup/SignupPage/SignupPage";
+import DashboardPage from "./DashboardPage/DashboardPage";
+import ProfilePage from "./ProfilePage/ProfilePage";
+import StudioPage from "./StudioPage/StudioPage";
 
 import "./App.scss";
 
 const App = () => {
-  const loggedIn = false;
+  const loggedIn = true;
 
   const routes = (
     <Switch>
       <Route path="/login" component={LoginPage}></Route>
-      {/* <Route path="/signup" component={SignupPage}></Route> */} */}
+      <Route path="/signup" component={SignupPage}></Route>
       <Route path="/" exact component={LandingPage}></Route>
       <Redirect to="/" />
     </Switch>
@@ -21,12 +25,10 @@ const App = () => {
 
   const guardedRoutes = (
     <Switch>
-      {/* <Route path="/dashboard" component={Dashboard} />
-      <Route path="/categories" component={CategoriesPage} />
-      <Route path="/personal-categories" component={PersonalCategoriesPage} />
-      <Route path="/all-spendings" component={AllSpendingsPage} />
-      <Route path="/settings" component={SettingsPage} /> */}
-      {/* <Redirect to="/dashboard" /> */}
+      <Route path="/dashboard" component={DashboardPage} />
+      <Route path="/studio" component={StudioPage} />
+      <Route path="/profile" component={ProfilePage} />
+      <Redirect to="/dashboard" />
     </Switch>
   );
 
---
+++ src/App/ProfilePage/ProfilePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProfilePage = () => (
+  <div className="profile-page">
+    <h1>Profile Page</h1>
+  </div>
+);
+
+export default ProfilePage;
