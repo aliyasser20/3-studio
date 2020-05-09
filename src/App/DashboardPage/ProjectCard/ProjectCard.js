e9c7d89290ed1d260f498ff0ff2cf2d1d3dfe89d
aliyasser20 <aliyasser20@hotmail.com>
2020-05-09 17:55:13 -0400

    cancel button, delete modal work in progress

--- src/App/DashboardPage/ProjectCard/ProjectCard.js
+++ src/App/DashboardPage/ProjectCard/ProjectCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import {
@@ -18,68 +18,98 @@ import SwipePictures from "../../UI/SwipePictures/SwipePictures";
 
 import "./ProjectCard.scss";
 
-const ProjectCard = props => (
-  <div className="project-card">
-    <Card classes={{ root: "single-card" }}>
-      <SwipePictures pictures={props.screenshots.slice(0, 3)} />
-      <CardContent classes={{ root: "content-area" }}>
-        <Typography gutterBottom variant="h6" component="h2">
-          <Box fontWeight={700}>{props.name}</Box>
-        </Typography>
-        <Typography
-          variant="body2"
-          component="p"
-          classes={{ root: "project-description" }}
-        >
-          {props.description}
-        </Typography>
-        <Divider
-          variant="fullWidth"
-          classes={{ root: "horizontal-divider-upper" }}
-        />
-        <span className="section">
-          <span className="dates">
-            <Box fontWeight={500}>
-              <Typography variant="caption" component="p">
-                Created: Jan 2, 2019
-              </Typography>
-            </Box>
-            <Typography variant="caption" component="p">
-              Updated: Mar 4, 2020
-            </Typography>
-          </span>
-          <span className="action-buttons">
-            <IconButton
-              aria-label="edit"
-              classes={{ root: "action-button" }}
-              size="small"
-            >
-              <EditIcon />
-            </IconButton>
-            <IconButton
-              aria-label="delete"
-              classes={{ root: "action-button" }}
-              size="small"
-            >
-              <DeleteIcon />
-            </IconButton>
-          </span>
-        </span>
-        <Divider variant="fullWidth" classes={{ root: "horizontal-divider" }} />
+const ProjectCard = props => {
+  const [confirmDelete, setConfirmDelete] = useState(true);
+
+  const confirmDeleteModal = (
+    <div className="confirm-delete-modal">
+      <Typography gutterBottom variant="subtitle1">
+        <Box fontWeight={700}>
+          Are you sure you want to permanently delete this project?
+        </Box>
+      </Typography>
+      <div className="confirm-actions">
         <span className="gradient-button">
-          <Button
-            classes={{ root: "open-in-studio" }}
-            variant="contained"
-            color="primary"
-            startIcon={<CameraIcon />}
-          >
-            Open in Studio
+          <Button variant="contained" color="primary">
+            Delete
           </Button>
         </span>
-      </CardContent>
-    </Card>
-  </div>
-);
+        <span className="cancel-button">
+          <Button variant="contained" color="primary">
+            Cancel
+          </Button>
+        </span>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="project-card">
+      <Card classes={{ root: "single-card" }}>
+        <SwipePictures pictures={props.screenshots.slice(0, 3)} />
+        <CardContent classes={{ root: "content-area" }}>
+          <Typography gutterBottom variant="h6" component="h2">
+            <Box fontWeight={700}>{props.name}</Box>
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p"
+            classes={{ root: "project-description" }}
+          >
+            {props.description}
+          </Typography>
+          <Divider
+            variant="fullWidth"
+            classes={{ root: "horizontal-divider-upper" }}
+          />
+          <span className="section">
+            <span className="dates">
+              <Box fontWeight={500}>
+                <Typography variant="caption" component="p">
+                  Created: Jan 2, 2019
+                </Typography>
+              </Box>
+              <Typography variant="caption" component="p">
+                Updated: Mar 4, 2020
+              </Typography>
+            </span>
+            <span className="action-buttons">
+              <IconButton
+                aria-label="edit"
+                classes={{ root: "action-button" }}
+                size="small"
+              >
+                <EditIcon />
+              </IconButton>
+              <IconButton
+                aria-label="delete"
+                classes={{ root: "action-button" }}
+                size="small"
+              >
+                <DeleteIcon />
+              </IconButton>
+            </span>
+          </span>
+          <Divider
+            variant="fullWidth"
+            classes={{ root: "horizontal-divider" }}
+          />
+          <span className="gradient-button">
+            <Button
+              classes={{ root: "open-in-studio" }}
+              variant="contained"
+              color="primary"
+              startIcon={<CameraIcon />}
+            >
+              Open in Studio
+            </Button>
+          </span>
+        </CardContent>
+      </Card>
+      {confirmDelete && confirmDeleteModal}
+    </div>
+  );
+};
 
 ProjectCard.propTypes = {
   name: PropTypes.string.isRequired,
