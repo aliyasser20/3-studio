73abe73c960bfbd8d47a48ecfdd7d3e152289703
aliyasser20 <aliyasser20@hotmail.com>
2020-05-09 16:56:28 -0400

    loader added

---
+++ src/App/UI/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const Loader = () => (
+  <div className="loader">
+    <CircularProgress />
+  </div>
+);
+
+export default Loader;
