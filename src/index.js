1dbf09b3d96ce0cbe6bc4b652e72b15a7ebba1b4
aliyasser20 <aliyasser20@hotmail.com>
2020-05-10 18:50:30 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { createStore, compose, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 import config from "./auth_config.json";
