b93ddb200cade757242b490daf0169622bb04ec9
aliyasser20 <aliyasser20@hotmail.com>
2020-05-10 19:27:31 -0400

    update

--- src/App/DashboardPage/DashboardPage.js
+++ src/App/DashboardPage/DashboardPage.js
@@ -33,28 +33,31 @@ const DashboardPage = props => {
     setSnackBarOpen(true);
   };
 
-  // const getProjects = () => {
-  //   const formData = new FormData();
-  //   // formData.append("file", files[0]);
-  //   // formData.append("tags", "rocket");
-  //   // formData.append("upload_preset", "modelUpload"); // Replace the preset name with your own
-  //   // formData.append("api_key", "463438241363482"); // Replace API key with your own Cloudinary key
-  //   // formData.append("api_secert", "tq2iVv3TvB-O8JRKH0FRGjtkWrs"); // Replace API key with your own Cloudinary key
-  //   // formData.append("timestamp", (Date.now() / 1000) | 0);
-  //   console.log(formData);
-  //   return cloudinaryAxios
-  //     .get("/resources/search", formData, {
-  //       // headers: { "X-Requested-With": "XMLHttpRequest" }
-  //       headers: {
-  //         "Content-Type": "application/json"
-  //       }
-  //     })
-  //     .then(res => {
-  //       console.log(res);
-  //     });
-  // };
+  const getProjects = () => {
+    const formData = new FormData();
+    // formData.append("file", files[0]);
+    // formData.append("tags", "rocket");
+    // formData.append("upload_preset", "modelUpload"); // Replace the preset name with your own
+    formData.append("api_key", "463438241363482"); // Replace API key with your own Cloudinary key
+    formData.append("api_secert", "tq2iVv3TvB-O8JRKH0FRGjtkWrs"); // Replace API key with your own Cloudinary key
+    // formData.append("timestamp", (Date.now() / 1000) | 0);
+    console.log(formData);
+    return cloudinaryAxios
+      .get("/resources", formData, {
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+          "Content-Type": "application/json"
+        }
+        // headers: {
+        //   "Content-Type": "application/json"
+        // }
+      })
+      .then(res => {
+        console.log(res);
+      });
+  };
 
-  // getProjects();
+  getProjects();
 
   const projectCards = props.allProjects.map(project => (
     <ProjectCard
