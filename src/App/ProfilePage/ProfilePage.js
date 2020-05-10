8a8d5248735ebe411efcec0a67da821e5fbd8403
aliyasser20 <aliyasser20@hotmail.com>
2020-05-10 04:41:03 -0400

    connect redux to profile page component

--- src/App/ProfilePage/ProfilePage.js
+++ src/App/ProfilePage/ProfilePage.js
@@ -1,20 +1,39 @@
 import React from "react";
-import { useAuth0 } from "../../react-auth0-spa";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const ProfilePage = () => {
-  const { loading, user } = useAuth0();
-  if (loading || !user) {
-    return <div>Loading...</div>;
-  }
+import { Container } from "@material-ui/core";
+
+import { useAuth0 } from "../../react-auth0-spa";
+import * as actions from "../../store/actions/index";
+
+const ProfilePage = props => {
+  const { user } = useAuth0();
+
   return (
     <div className="profile-page">
-      <h1>Profile Page</h1>
-      <img src={user.picture} alt="Profile" />
-      <h2>Testing id (sub): {user.sub}</h2>
-      <h2>Name of user: {user.name}</h2>
-      <p>Email of user: {user.email}</p>
+      <Container maxWidth="xl" classes={{ root: "container-padding" }}>
+        <h1>Profile Page</h1>
+        <img src={user.picture} alt="Profile" />
+        <h2>Testing id (sub): {user.sub}</h2>
+        <h2>Name of user: {user.name}</h2>
+        <p>Email of user: {user.email}</p>
+      </Container>
     </div>
   );
 };
 
-export default ProfilePage;
+ProfilePage.propTypes = {
+  currentTheme: PropTypes.string.isRequired,
+  onSetTheme: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  currentTheme: state.themes.currentTheme
+});
+
+const mapDispatchToProps = dispatch => ({
+  onSetTheme: theme => dispatch(actions.setTheme(theme))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
