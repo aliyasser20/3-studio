b00b85d4ff3e15361ed4057025c99ac3ff02627b
aliyasser20 <aliyasser20@hotmail.com>
2020-05-12 02:03:01 -0400

    design for mode selector complete

--- src/App/StudioPage/StudioTopBar/ModeSelector/ModeSelector.js
+++ src/App/StudioPage/StudioTopBar/ModeSelector/ModeSelector.js
@@ -3,17 +3,37 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { Button, ButtonGroup } from "@material-ui/core";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import BuildRoundedIcon from "@material-ui/icons/BuildRounded";
+
+import Edit from "../../../UI/SVGIcons/Edit";
 
 import * as actions from "../../../../store/actions/index";
+
+import "./ModeSelector.scss";
 
 const ModeSelector = props => {
   let modes;
   return (
     <div className="modes-selector">
-      <ButtonGroup color="primary" aria-label="outlined primary button group">
-        <Button>View</Button>
-        <Button>Edit</Button>
-        <Button>Media</Button>
+      <ButtonGroup
+        classes={{ root: "mode-buttons" }}
+        color="primary"
+        aria-label="outlined primary button group"
+      >
+        <Button classes={{ root: "mode-button" }}>
+          <VisibilityIcon fontSize="small" />
+          View
+        </Button>
+        <Button classes={{ root: "mode-button" }}>
+          <Edit />
+          Edit
+        </Button>
+        <Button classes={{ root: "mode-button" }}>
+          <VideocamIcon fontSize="small" />
+          Media
+        </Button>
       </ButtonGroup>
     </div>
   );
