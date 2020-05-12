038b4129733b56a5bc494cdcf7720f09e3db56c4
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-12 01:09:27 -0400

    add basic profile page to be continued

---
+++ src/App/ProfilePage/UserInfoTable/UserInfoTable.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { Table, TableBody, TableContainer, Paper } from "@material-ui/core";
+
+import Row from "./Row/Row";
+
+import { useAuth0 } from "../../../react-auth0-spa";
+
+export default function CustomizedTables() {
+  const { user } = useAuth0();
+  const [editName, setEditName] = useState(false);
+  const [editNickname, setEditNickname] = useState(false);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="user info table">
+        <TableBody>
+          <Row
+            value={user.name}
+            field="Name"
+            edit={editName}
+            setEdit={setEditName}
+          />
+          <Row
+            value={user.nickname}
+            field="Nickname"
+            edit={editNickname}
+            setEdit={setEditNickname}
+          />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
