35a557cf412947e02aaf9afcfda8d7e6a1d068e1
aliyasser20 <aliyasser20@hotmail.com>
2020-05-12 01:21:31 -0400

    modes selector add to studio top bar

--- src/App/StudioPage/StudioTopBar/StudioTopBar.js
+++ src/App/StudioPage/StudioTopBar/StudioTopBar.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 
 import { Paper, Box, Typography, ThemeProvider } from "@material-ui/core";
 
+import ModeSelector from "./ModeSelector/ModeSelector";
+
 import themeCreator from "../../../helpers/themeCreator";
 
 import "./StudioTopBar.scss";
@@ -15,11 +17,19 @@ const StudioTopBar = props => {
     <ThemeProvider theme={theme}>
       <div className="studio-top-bar">
         <Paper classes={{ root: "custom-paper" }} elevation={3}>
-          <Box fontWeight={500}>
-            <Typography variant="h6" color="primary" component="p">
-              {props.currentProject.name}
-            </Typography>
-          </Box>
+          <div className="top-bar-grid">
+            <div className="top-bar-left">
+              <Box fontWeight={500}>
+                <Typography variant="h6" color="primary" component="p">
+                  {props.currentProject.name}
+                </Typography>
+              </Box>
+            </div>
+            <div className="top-bar-center">
+              <ModeSelector />
+            </div>
+            <div className="top-bar-right"></div>
+          </div>
         </Paper>
       </div>
     </ThemeProvider>
