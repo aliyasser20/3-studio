b063ac346d6ce81d6b17adf549505371af8305d2
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-12 04:42:48 -0400

    profile page work in progress

--- src/App/ProfilePage/UserInfoTable/Row/Row.js
+++ src/App/ProfilePage/UserInfoTable/Row/Row.js
@@ -7,52 +7,13 @@ import CheckIcon from "@material-ui/icons/Check";
 import CloseIcon from "@material-ui/icons/Close";
 
 import { useAuth0 } from "../../../../react-auth0-spa";
-
-const request = require("request");
-
-// Get access token
-
-const optionsGetAccessToken = {
-  method: "POST",
-  url: "https://dev-1ee5do6a.auth0.com/oauth/token",
-  headers: { "content-type": "application/x-www-form-urlencoded" },
-  form: {
-    grant_type: "client_credentials",
-    client_id: "IysHD3AMe37ZsxVf0cW9TfVDMDjJ0VHv",
-    client_secret:
-      "UEuwfEq5RAB9dy-Y_Hs_urR61brt8BwU4ySX1yIdYJO9UtpKy33HteFrorblfXKH",
-    audience: "https://dev-1ee5do6a.auth0.com/api/v2/"
-  }
-};
-
-request(optionsGetAccessToken, function(error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
+import backendAxios from "../../../../axiosInstances/backendAxios";
 
 const Row = props => {
   const { user } = useAuth0();
   const handleCheck = () => {
-    // send api to update field.then():
-    const options = {
-      method: "PATCH",
-      url: `https://dev-1ee5do6a.auth0.com/api/v2/users/${user.sub.slice(6)}`,
-      headers: {
-        "content-type": "application/json",
-        authorization: "Bearer MGMT_API_ACCESS_TOKEN", // GET THIS from above
-        "cache-control": "no-cache"
-      },
-      body: '{ "name": {NAME_VALUE}, "nickname": NICKNAME_VALUE}' // GET CURRENT NAME AND NICKNAME
-    };
-
-    request(options, (error, response, body) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        props.setEdit(false);
-      }
-      console.log(body);
+    backendAxios.put("/api/users", {
+      userId: user.sub
     });
   };
 
