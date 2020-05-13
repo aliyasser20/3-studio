e8f2fb07bf91daf16c3848227592da850f4a43df
Jaybur1 <jayburbyga@gmail.com>
2020-05-13 10:17:06 -0400

    created mediaState actions

--- src/store/actions/mediaState.js
+++ src/store/actions/mediaState.js
@@ -1,6 +1,31 @@
 import * as actionTypes from "./actionTypes";
 
-export const setMediaState = (mediaState) => ({
-  action: actionTypes.SET_MEDIA_STATE,
-  mediaState,
+export const setMediaModle = (mediaModel) => ({
+  action: actionTypes.SET_MEDIA_MODEL,
+  mediaModel,
+});
+
+export const setMediaFov = (mediaFov) => ({
+  type: actionTypes.SET_MEDIA_FOV,
+  mediaFov,
+});
+
+export const setMediaFar = (mediaFar) => ({
+  type: actionTypes.SET_MEDIA_FAR,
+  mediaFar,
+});
+
+export const setMediaNear = (mediaNear) => ({
+  type: actionTypes.SET_MEDIA_NEAR,
+  mediaNear,
+});
+
+export const setMediaSizeBounding = (mediaSizeBounding) => ({
+  type: actionTypes.SET_MEDIA_SIZE_BOUNDING,
+  mediaSizeBounding,
+});
+
+export const setMediaBox = (mediaBox) => ({
+  type: actionTypes.SET_MEDIA_BOX,
+  mediaBox,
 });
