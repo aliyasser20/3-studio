8911c396960252dcbbe62e1151d6c5bcd113994d
aliyasser20 <aliyasser20@hotmail.com>
2020-05-14 17:29:55 -0400

    add materials to appearance bar

--- src/App/StudioPage/AppearancesBar/materials.js
+++ src/App/StudioPage/AppearancesBar/materials.js
@@ -19,5 +19,131 @@ export const materials = [
       "https://res.cloudinary.com/aajfinal/image/upload/v1589488604/appearances/picture%20assets/purplePolished_wfory2.png",
     group: "metals",
     actionName: "purplePolished"
+  },
+  {
+    name: "aluminum polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490718/appearances/picture%20assets/alumPolished_s9jm9g.png",
+    group: "metals",
+    actionName: "aluminumPolished"
+  },
+  {
+    name: "steel polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490720/appearances/picture%20assets/steelPolished_j8yy2z.png",
+    group: "metals",
+    actionName: "steelPolished"
+  },
+  {
+    name: "graphite polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/graphitePolished_tbicbl.png",
+    group: "metals",
+    actionName: "graphitePolished"
+  },
+  {
+    name: "light blue polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490721/appearances/picture%20assets/lightBluePol_sz7tg0.png",
+    group: "metals",
+    actionName: "lightBluePolished"
+  },
+  {
+    name: "light green polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490720/appearances/picture%20assets/lightGreenPol_rsez4p.png",
+    group: "metals",
+    actionName: "lightGreenPolished"
+  },
+  {
+    name: "green polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/greenPolished_pvgn8i.png",
+    group: "metals",
+    actionName: "greenPolished"
+  },
+  {
+    name: "blue polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490718/appearances/picture%20assets/bluePolished_a0xilz.png",
+    group: "metals",
+    actionName: "bluePolished"
+  },
+  {
+    name: "blue rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490718/appearances/picture%20assets/blueRog_inaafd.png",
+    group: "metals",
+    actionName: "blueRough"
+  },
+  {
+    name: "green rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/greenRog_hshm0y.png",
+    group: "metals",
+    actionName: "greenRough"
+  },
+  {
+    name: "light green rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490720/appearances/picture%20assets/lightGreenRog_paecof.png",
+    group: "metals",
+    actionName: "lightGreenRough"
+  },
+  {
+    name: "cherry rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490722/appearances/picture%20assets/cherryRog_r8oykg.png",
+    group: "metals",
+    actionName: "cherryRough"
+  },
+  {
+    name: "purple rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490720/appearances/picture%20assets/purpleRog_qpk2od.png",
+    group: "metals",
+    actionName: "purpleRough"
+  },
+  {
+    name: "brass rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490718/appearances/picture%20assets/brassRog_psbfda.png",
+    group: "metals",
+    actionName: "brassRough"
+  },
+  {
+    name: "brass polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490718/appearances/picture%20assets/brassPolished_piturj.png",
+    group: "metals",
+    actionName: "brassPolished"
+  },
+  {
+    name: "graphite rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/graphiteRog_ngg4lc.png",
+    group: "metals",
+    actionName: "graphiteRough"
+  },
+  {
+    name: "copper rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/copperRog_sjohxs.png",
+    group: "metals",
+    actionName: "copperRough"
+  },
+  {
+    name: "copper polished",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490719/appearances/picture%20assets/copperPolished_kt13ax.png",
+    group: "metals",
+    actionName: "copperPolished"
+  },
+  {
+    name: "steel rough",
+    imgPath:
+      "https://res.cloudinary.com/aajfinal/image/upload/v1589490721/appearances/picture%20assets/steelRog_qwt6fg.png",
+    group: "metals",
+    actionName: "steelRough"
   }
 ];
