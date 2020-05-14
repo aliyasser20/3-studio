61fd893e5f5e186083fdf206b7361e96580c31b0
aliyasser20 <aliyasser20@hotmail.com>
2020-05-14 14:58:47 -0400

    fix object button background

--- src/App/StudioPage/AppearancesBar/AppearancesBar.js
+++ src/App/StudioPage/AppearancesBar/AppearancesBar.js
@@ -2,21 +2,67 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { Paper, ThemeProvider } from "@material-ui/core";
+import {
+  Paper,
+  ThemeProvider,
+  Tabs,
+  Tab,
+  Box,
+  Typography
+} from "@material-ui/core";
 
 import themeCreator from "../../../helpers/themeCreator";
 import * as actions from "../../../store/actions/index";
 
 import "./AppearancesBar.scss";
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`
+  };
+}
+
 const AppearancesBar = props => {
   const theme = themeCreator("#ffffff", "#212121");
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="appearances-bar">
         <Paper classes={{ root: "custom-paper" }} elevation={3}>
-          <div
+          {/* <div
             draggable
             onDragStart={e => {
               e.dataTransfer.dropEffect = "link";
@@ -41,8 +87,33 @@ const AppearancesBar = props => {
             className="material purple"
           >
             Polished Purple
-          </div>
+          </div> */}
         </Paper>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Item Four" {...a11yProps(3)} />
+          <div className="">
+            <TabPanel value={value} index={0}>
+              Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              Item Three
+            </TabPanel>
+          </div>
+        </Tabs>
       </div>
     </ThemeProvider>
   );
