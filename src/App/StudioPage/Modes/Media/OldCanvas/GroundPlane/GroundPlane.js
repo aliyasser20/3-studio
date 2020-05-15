e105808e6b80644057bd86df7973097cbb212288
Jaybur1 <jayburbyga@gmail.com>
2020-05-14 20:24:56 -0400

    material testing

--- src/App/StudioPage/Modes/Media/OldCanvas/GroundPlane/GroundPlane.js
+++ src/App/StudioPage/Modes/Media/OldCanvas/GroundPlane/GroundPlane.js
@@ -6,13 +6,13 @@ const GroundPlane = () => {
   useEffect(() => {
     console.log(mesh.current);
     if (mesh.current) {
-      mesh.current.material = materialLibrary().goldPolished;
+      mesh.current.material = materialLibrary().wallMedieval;
     }
   });
 
   return (
     <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]} ref={mesh}>
-      <planeBufferGeometry attach="geometry" args={[5, 5]} />
+      <planeBufferGeometry attach="geometry" args={[500, 500]} />
       {/* <meshPhysicalMaterial attach="material" /> */}
     </mesh>
   );
