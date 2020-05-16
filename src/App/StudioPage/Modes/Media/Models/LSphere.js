e64652c9cffd337ac6b3e864f8fd2274c0369c23
Jaybur1 <jayburbyga@gmail.com>
2020-05-16 19:45:05 -0400

    creating  lSphere

---
+++ src/App/StudioPage/Modes/Media/Models/LSphere.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from "react";
+import { useFrame } from "react-three-fiber";
+
+const LSphere = (props) => {
+  const lSphere = useRef();
+  useEffect(() => {
+    const dragObjs = [...props.dragObjects];
+
+    lSphere.current && props.setDrag([...dragObjs, lSphere.current]);
+  }, []);
+  useFrame(({ gl, scene, camera }) => {
+    gl.render(scene, camera);
+    if (props.lSphere.rotateX) lSphere.current.rotation.x += 0.007;
+    if (props.lSphere.rotateY) lSphere.current.rotation.y += 0.007;
+    if (props.lSphere.rotateZ) lSphere.current.rotation.z += 0.007;
+  });
+  return (
+    <mesh
+      ref={lSphere}
+      name="l-sphere"
+      visible
+      userData={{ name: "l-sphere" }}
+      onPointerOver={(e) => props.toggleMediaLock()}
+      onPointerOut={(e) => props.toggleMediaLock()}
+      castShadow
+      scale={[...props.lSphere.scale]}
+    >
+      <sphereGeometry attach="geometry" args={[...props.lSphere.args]} />
+      {/* <meshStandardMaterial
+        attach="material"
+        color={`#${props.lSphere.color}`}
+        transparent
+        roughness={1}
+        metalness={0}
+      /> */}
+    </mesh>
+  );
+};
+
+export default LSphere;
