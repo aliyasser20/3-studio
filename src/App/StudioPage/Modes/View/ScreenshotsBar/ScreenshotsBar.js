5662c1f9dea7fd193fab09feffc79050f76021bb
aliyasser20 <aliyasser20@hotmail.com>
2020-05-17 23:45:07 -0400

    screenshot viewer complete

--- src/App/StudioPage/Modes/View/ScreenshotsBar/ScreenshotsBar.js
+++ src/App/StudioPage/Modes/View/ScreenshotsBar/ScreenshotsBar.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { Paper } from "@material-ui/core";
+import { Paper, Dialog } from "@material-ui/core";
 
 import backendAxios from "../../../../../axiosInstances/backendAxios";
 
 import "./ScreenshotsBar.scss";
 
 const ScreenshotsBar = props => {
-  let classes;
   const [screenshots, setScreenshots] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
+    console.log("here");
     backendAxios
       .get("/api/screenshots", {
         params: {
@@ -25,14 +27,16 @@ const ScreenshotsBar = props => {
       .catch(err => console.log(err));
   }, [props.currentProject.id]);
 
-  console.log(screenshots);
-
   const screenshotsComponents = screenshots.map(screenshot => (
     <img
       className="screenshot-image"
       key={screenshot.path}
       src={screenshot.path}
       alt={screenshot.label}
+      onClick={() => {
+        setImage(screenshot);
+        setOpenDialog(true);
+      }}
     />
   ));
 
@@ -41,6 +45,26 @@ const ScreenshotsBar = props => {
       <Paper classes={{ root: "custom-paper" }} elevation={3}>
         <div className="screenshots">{screenshotsComponents}</div>
       </Paper>
+      <Dialog
+        classes={{ root: "screenshot-view-dialog" }}
+        open={openDialog}
+        onClose={() => {
+          setImage(null);
+          setOpenDialog(false);
+        }}
+        aria-labelledby="Update picture"
+      >
+        {image && (
+          <div className="image-container">
+            <img
+              className="big-screenshot-image"
+              src={image.path}
+              alt={image.label}
+            />
+            <p className="label">{image.label}</p>
+          </div>
+        )}
+      </Dialog>
     </div>
   );
 };
