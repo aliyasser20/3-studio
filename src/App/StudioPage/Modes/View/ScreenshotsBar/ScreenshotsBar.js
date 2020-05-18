b46ae175ae3dd84f6b56a2d04569d1bf0898e143
aliyasser20 <aliyasser20@hotmail.com>
2020-05-17 22:55:21 -0400

    screenshots get and display on screenshots bar

--- src/App/StudioPage/Modes/View/ScreenshotsBar/ScreenshotsBar.js
+++ src/App/StudioPage/Modes/View/ScreenshotsBar/ScreenshotsBar.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { Paper } from "@material-ui/core";
+
+import backendAxios from "../../../../../axiosInstances/backendAxios";
+
+import "./ScreenshotsBar.scss";
 
 const ScreenshotsBar = props => {
   let classes;
-  return <div className="screenshots-bar"></div>;
+  const [screenshots, setScreenshots] = useState([]);
+
+  useEffect(() => {
+    backendAxios
+      .get("/api/screenshots", {
+        params: {
+          projectId: props.currentProject.id
+        }
+      })
+      .then(resp => {
+        setScreenshots(resp.data.screenshots);
+      })
+      .catch(err => console.log(err));
+  }, [props.currentProject.id]);
+
+  console.log(screenshots);
+
+  const screenshotsComponents = screenshots.map(screenshot => (
+    <img
+      className="screenshot-image"
+      key={screenshot.path}
+      src={screenshot.path}
+      alt={screenshot.label}
+    />
+  ));
+
+  return (
+    <div className="screenshots-bar">
+      <Paper classes={{ root: "custom-paper" }} elevation={3}>
+        <div className="screenshots">{screenshotsComponents}</div>
+      </Paper>
+    </div>
+  );
 };
 
-ScreenshotsBar.propTypes = {};
+ScreenshotsBar.propTypes = {
+  currentProject: PropTypes.object.isRequired
+};
 
-export default ScreenshotsBar;
+const mapStateToProps = state => ({
+  currentProject: state.projects.currentProject
+});
+
+export default connect(mapStateToProps, null)(ScreenshotsBar);
--- src/store/reducers/modeControl.js
+++ src/store/reducers/modeControl.js
@@ -1,7 +1,7 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-  currentMode: "EDIT"
+  currentMode: "VIEW"
 };
 
 const reducer = (state = initialState, action) => {
